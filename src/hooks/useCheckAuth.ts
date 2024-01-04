2b97bd1b88035430381aa2d5c915cf9a0fa958d6
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2024-01-04 15:56:23 +0700

    comment unsue hook

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -1,45 +1,45 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
-import { useSession } from 'next-auth/react'
+// import { useSession } from 'next-auth/react'
 
-export const useCheckAuth = () => {
-  const router = useRouter()
+// export const useCheckAuth = () => {
+//   const router = useRouter()
 
-  const { status, data } = useSession()
-  const isAdmin = data?.user.isAdmin || false
+//   const { status, data } = useSession()
+//   const isAdmin = data?.user.isAdmin || false
 
-  useEffect(() => {
-    // logged in but go to user modifier pages => redirect to internal page
-    if (
-      status === 'authenticated' &&
-      (router.route === '/auth/signin' ||
-        router.route === '/register' ||
-        router.route === '/forgot-password' ||
-        router.route === '/change-password')
-    ) {
-      isAdmin ? router.replace('/dashboard') : router.replace('/')
-    } else if (
-      // not logged in and don't go to user modifier pages => if go to normal page => kick to signin page
-      status === 'unauthenticated' &&
-      router.route !== '/auth/signin' &&
-      router.route !== '/register' &&
-      router.route !== '/forgot-password' &&
-      router.route !== '/change-password'
-    ) {
-      router.replace('/auth/signin')
-    }
+//   useEffect(() => {
+//     // logged in but go to user modifier pages => redirect to internal page
+//     if (
+//       status === 'authenticated' &&
+//       (router.route === '/auth/signin' ||
+//         router.route === '/register' ||
+//         router.route === '/forgot-password' ||
+//         router.route === '/change-password')
+//     ) {
+//       isAdmin ? router.replace('/dashboard') : router.replace('/')
+//     } else if (
+//       // not logged in and don't go to user modifier pages => if go to normal page => kick to signin page
+//       status === 'unauthenticated' &&
+//       router.route !== '/auth/signin' &&
+//       router.route !== '/register' &&
+//       router.route !== '/forgot-password' &&
+//       router.route !== '/change-password'
+//     ) {
+//       router.replace('/auth/signin')
+//     }
 
-    // protect admin page
-    if (status === 'authenticated' && !isAdmin) {
-      router.replace('/')
-    }
-  }, [status])
+//     // protect admin page
+//     if (status === 'authenticated' && !isAdmin) {
+//       router.replace('/')
+//     }
+//   }, [status])
 
-  return {
-    status,
-    isAuthenticated: status === 'authenticated',
-    isAdmin
-  }
-}
+//   return {
+//     status,
+//     isAuthenticated: status === 'authenticated',
+//     isAdmin
+//   }
+// }
