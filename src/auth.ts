9ed8ca27f59e9381edd3103bd620b2bda99a65a6
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2024-01-06 20:57:33 +0700

    rm getUser of auth

--- src/auth.ts
+++ src/auth.ts
@@ -5,19 +5,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import UserModel, { User } from './models/User';
 
-// import { sql } from '@vercel/postgres';
-// import type { User } from '@/app/lib/definitions';
 // import bcrypt from 'bcrypt';
-
-// async function getUser(email: string): Promise<User | undefined> {
-//     try {
-//         const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-//         return user.rows[0];
-//     } catch (error) {
-//         console.error('Failed to fetch user:', error);
-//         throw new Error('Failed to fetch user.');
-//     }
-// }
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
