84a752daf7aaa2b43abdd381e69d0c3ac67016f4
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2025-12-25 12:20:37 +0700

    add route group for admin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
+import LandingPage from '@/components/landing/LandingPage'
+
 const HomePage = () => {
-  return (
-    <div className='bg-blue-500 text-white text-2xl p-6 rounded-lg'>
-      Nếu bạn thấy nền xanh dương → Tailwind đã hoạt động ✅
-    </div>
-  )
+  return <LandingPage />
 }
 
 export default HomePage
