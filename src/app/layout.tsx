107fae14334fa8889576a85a6b88f3b737ac9a58
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2026-01-04 08:36:05 +0700

    add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,22 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: 'TheTapHoa',
-  description: 'TheTapHoa, TPHCM, Viet Nam'
+  description: 'TheTapHoa, TPHCM, Viet Nam',
+  manifest: '/favicon_io/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon_io/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon_io/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon_io/favicon.ico', sizes: 'any' }
+    ],
+    apple: [
+      { url: '/favicon_io/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      { rel: 'android-chrome-192x192', url: '/favicon_io/android-chrome-192x192.png' },
+      { rel: 'android-chrome-512x512', url: '/favicon_io/android-chrome-512x512.png' }
+    ]
+  }
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
