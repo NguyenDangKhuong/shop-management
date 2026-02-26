4d9348f908278daa6ec7d4a9413b098eafeccf9b
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2026-02-26 11:01:22 +0700

    feat: restrict xvn.vercel.app to tweets-only, anonymize pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,13 @@
 import Link from 'next/link'
+import { Metadata } from 'next'
 
 import { Button, Result } from 'antd'
+
+export const metadata: Metadata = {
+  title: 'Not Found',
+  description: 'Page not found',
+  icons: [],
+}
 
 const NotFoundPage = () => {
   return (
@@ -9,8 +16,8 @@ const NotFoundPage = () => {
       title='404'
       subTitle='Sorry, the page you visited does not exist.'
       extra={
-        <Link href='/'>
-          <Button type='primary'>Back Home</Button>
+        <Link href='/tweets'>
+          <Button type='primary'>Go to Tweets</Button>
         </Link>
       }
     />
