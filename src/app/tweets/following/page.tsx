3264d901c7734ebcce9cafa681b8e0831a8cb3b4
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2026-06-14 14:30:34 +0700

    refactor: move tweets feature to private git submodule

--- src/app/tweets/following/page.tsx
+++
@@ -1,5 +0,0 @@
-import { HomeFeed } from '../HomeFeed'
-
-export default function FollowingPage() {
-    return <HomeFeed tab="following" />
-}
--- src/app/tweets/for-you/page.tsx
+++
@@ -1,5 +0,0 @@
-import { HomeFeed } from '../HomeFeed'
-
-export default function ForYouPage() {
-    return <HomeFeed tab="for_you" />
-}
